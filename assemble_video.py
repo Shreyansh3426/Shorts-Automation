54d5c5d54813ac0e7557c3199b2919c2b435f3b3
Shreyansh3426 <shreyanshp2005@gmail.com>
2026-03-20 18:28:29 +0530

    Viral shorts captions: 28pt, clean bottom style

--- assemble_video.py
+++ assemble_video.py
@@ -80,14 +80,14 @@ def assemble_video(topic_id, clips_json, voice_path, output_path):
     srt_lines = []
     ass_lines = []
 
-    # ASS HEADER (for styling) - BOTTOM CAPTIONS LIKE VIRAL SHORTS
+    # ASS HEADER - VIRAL SHORTS CAPTIONS (CLEAN & SMALL)
     ass_header = """
 [Script Info]
 ScriptType: v4.00+
 
 [V4+ Styles]
 Format: Name,Fontname,Fontsize,PrimaryColour,SecondaryColour,OutlineColour,BackColour,Bold,Italic,Underline,StrikeOut,ScaleX,ScaleY,Spacing,Angle,BorderStyle,Outline,Shadow,Alignment,MarginL,MarginR,MarginV,Encoding
-Style: Default,Arial Black,32,&H00FFFFFF,&H00FFFFFF,&H00000000,&H00000000,1,0,0,0,100,100,0,0,1,3,1,2,40,40,80,1
+Style: Default,Arial Black,28,&H00FFFFFF,&H00FFFFFF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,2,0,2,60,60,120,1
 
 [Events]
 Format: Layer,Start,End,Style,Name,MarginL,MarginR,MarginV,Effect,Text
